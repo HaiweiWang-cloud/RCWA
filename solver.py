2f28e433b27aed34659a67538a415166a92db470
Haiwei Wang <78714095+HaiweiWangUnimelb@users.noreply.github.com>
2024-04-03 19:05:32 +1100

    Added Device interface for computing RCWA solutions, yet to be fully implemented.

--- solver.py
+++ solver.py
@@ -1,4 +1,25 @@
 import numpy as np
+import rcwa
+import smatrix
+
+class Device:
+
+    def __init__(self) -> None:
+        self.uniform_layers = []
+        self.patterned_layers = []
+        self.stored_convolution_matrices = []
+        self.id_count = 0
+        
+    def set_k_space(self, Px, Py, k0_x, k0_y, M, N, alpha=np.pi/2):
+        self.K_x, self.K_y, self.p, self.q = create_k_space(Px, Py, k0_x, k0_y, M, N, alpha)
+
+    def add_layer_patterned(self, er_func):
+        self.patterned_layer.append({'er_fourier': get_Fourier_coefficients(er_func), 'id': self.id_count})
+        self.id_count += 1
+
+    def add_layer_uniform(self, er):
+        self.uniform_layers.append({'er': er, 'id': self.id_count})
+        self.id_count += 1
 
 def computeIncidentWave(wl, n_inc, theta, phi, p_TE, p_TM):
     '''
