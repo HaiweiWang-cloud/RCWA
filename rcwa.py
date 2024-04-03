3279453b17a7f38a9b178c671f5bb2b2845fbed1
Haiwei Wang <78714095+HaiweiWangUnimelb@users.noreply.github.com>
2024-04-03 17:17:51 +1100

    Implemented convolution matrix function

--- rcwa.py
+++ rcwa.py
@@ -13,6 +13,7 @@ def create_k_space(Px, Py, k0_x, k0_y, M, N, alpha=np.pi/2):
 
     Outputs:
     K_x, K_y - diagonal matrices with the wavevectors of the expansion rasterised.
+    P, Q - indices of the Floquet modes.
     '''
     p = np.arange(-M, M+1)
     q = np.arange(-N, N+1)
@@ -26,19 +27,25 @@ def create_k_space(Px, Py, k0_x, k0_y, M, N, alpha=np.pi/2):
         k_x = k0_x + 2*np.pi/Px * P
         k_y = k0_y + 2*np.pi/Py * Q - 2*np.pi/Px/np.tan(alpha) * P 
 
-    return np.diag(k_x.flatten()), np.diag(k_y.flatten())
+    return np.diag(k_x.flatten()), np.diag(k_y.flatten()), p, q
 
-def get_Fourier_coefficients(input, alpha=np.pi/2):
+def get_convolution_matrix(material_function, p, q):
     '''
     Inputs:
-        input - a square matrix corresponding to a function defined over the transformed orthogonal coordinates
+        material_function - sampled complex-valued function of the material over the lattice coordinate system. Size NxN, where N is a power of 2. 
+        p, q - plane wave expansion index vectors
     '''
+    N2 = material_function.size
+    N_half = int(material_function.shape[0]/2)
+    
+    a_mn = (1/N2) * np.fft.fftshift(np.fft.fft2(material_function))
 
-    return output
+    convolution_matrix = np.zeros((p.size * q.size, p.size * q.size), dtype=complex)
 
-def get_convolution_matrix(input, Gs):
-    '''
-    Inputs:
-        
-    '''
+    P, Q = np.meshgrid(p,q)
+
+    for i, p_i in enumerate(p):
+        for j, q_j in enumerate(q):
+            convolution_matrix[(j)*p.size + i, :] = a_mn[q_j-Q+N_half,p_i-P+N_half].flatten()
+
     return convolution_matrix
